f1977e73e20bf2deb6c230daa83b6afc05836c88
surinder sharma <suri@example.com>
2024-09-16 14:12:38 +0530

    Modified User data file

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 # shellcheck disable=SC2164
 cd /home/ubuntu
-yes | sudo apt update
-yes | sudo apt install python3 python3-pip
+sudo apt update
+sudo apt install python3 python3-pip
+#yes | sudo apt update
+#yes | sudo apt install python3 python3-pip
 git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
 sleep 20
 # shellcheck disable=SC2164
 cd python-mysql-db-proj-1
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --break-system-packages
 echo 'Waiting for 30 seconds before running the app.py'
 setsid python3 -u app.py &
 sleep 30
