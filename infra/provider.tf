b8426e0ed4fb623666070775edff4469579562ab
Surinder Sharma <surinder280@gmail.com>
2024-12-13 12:57:08 +0530

    Updated backened state

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,6 +1,13 @@
 provider "aws" {
-  region                   = "us-east-1"
+  region                   = "eu-central-1"
   shared_credentials_files = ["c://Users//surin//.aws/credentials"]
+}
+terraform {
+  backend "s3" {
+    bucket = "suritest123"
+    key    = "devops-project-1-terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 /*data "aws_secretsmanager_secret_version" "mysecret" {
